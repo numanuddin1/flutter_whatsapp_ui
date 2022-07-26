3dea7de57afbcfa505e211927199263e7df05813
Numan <nomanuddin2012@gmail.com>
2022-07-26 15:32:53 +0500

    status tab added

---
+++ lib/ui/tabs/status_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StatusTab extends StatelessWidget {
+  const StatusTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
